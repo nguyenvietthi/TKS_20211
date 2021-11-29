a8fda211c25b91448a044d9916de4ac2d2bad1b5
nguyenvietthi <nguyenvietthi.nd@gmail.com>
2021-11-29 13:47:06 +0700

    add comment RAMtoMEN

--- verilog/code/neuroset/RAMtoMEM.v
+++ verilog/code/neuroset/RAMtoMEM.v
@@ -54,74 +54,73 @@ assign step_out=step+step_n;
 assign address=firstaddr+i;
 
 
-always @(posedge clk)
-    begin
-		  if (GO==1) step=1; // GO -> layer 1 chay
-		  sh=sh+1; // sh = ~sh (sh co 2 state la 0 va 1)
-				if (step_out==1) // doc pixel input 
-				   begin
-					if ((i<=lastaddr-firstaddr)&&(sh==0))
-                    begin
-                        //address=firstaddr+i;
-								addr=i;
-								if (step_out==1) we_p=1; // write enable pixel (database)
-					end
-                if ((i<=lastaddr-firstaddr)&&(sh==1))
-                    begin
-                        if (we_p) // write data vao database
-									begin
-											addrp=addr;
-											dp=0;
-											dp[SIZE_1-1:0]=data;
-											we_p=0; // write unable pixel (database)
-									end
-									i=i+1;
-							end
-					 if ((i>lastaddr-firstaddr)&&(sh==1))
-                    begin
-                        step=step+1;          //next step
-                        i=0;
-                    end
-				end
-				if ((step_out==2)||(step_out==4)||(step_out==6)||(step_out==8)||(step_out==10)||(step_out==12)||(step_out==14))
-					begin
-						if ((i<=lastaddr-firstaddr)&&(sh==0))
-                    begin
-								addr=i1;
-						  end
-						if ((i<=lastaddr-firstaddr)&&(sh==1))
-                    begin
-										we_w=0;
-										addrw=addr;
-										if (weight_case!=0) i=i+1; 
-										if (step_out==14) if (i_d==(in_dense)) begin  dw=buff; we_w=1; weight_case=1; i_d=0; i1=i1+1; end
-										case (weight_case)
-											0: ;
-											1: begin buff=0; buff[SIZE_9-1:SIZE_8]=data; end   
-											2: buff[SIZE_8-1:SIZE_7]=data; 
-											3: buff[SIZE_7-1:SIZE_6]=data;  
-											4: buff[SIZE_6-1:SIZE_5]=data;  
-											5: buff[SIZE_5-1:SIZE_4]=data;           
-											6: buff[SIZE_4-1:SIZE_3]=data;  
-											7: buff[SIZE_3-1:SIZE_2]=data;  
-											8: buff[SIZE_2-1:SIZE_1]=data;   
-											9: begin buff[SIZE_1-1:0]=data;  i1=i1+1; end
-											default: $display("Check weight_case");
-										endcase
-										if (weight_case!=0) i_d=i_d+1;
-										if (weight_case==9) begin weight_case=1; dw=buff; we_w=1; end else weight_case=weight_case+1;
-										
-                    end
-						if ((i>lastaddr-firstaddr)&&(sh==1))
-                    begin
-                        step=step+1;          //next step
-                        i=0;
-								i_d=0;
-								i1=0;
-								weight_case=0;
-                    end
-            end
-				else
-					we_w=0;
-    end
+always @(posedge clk) begin
+	if (GO==1) step=1; // GO -> layer 1 chay
+	sh=sh+1; // sh = ~sh (sh co 2 state la 0 va 1)
+	if (step_out==1) begin // doc pixel input 
+		if ((i<=lastaddr-firstaddr)&&(sh==0)) begin
+			//address=firstaddr+i;
+			addr=i;
+			if (step_out==1) we_p=1; // write enable pixel (database)
+		end
+		if ((i<=lastaddr-firstaddr)&&(sh==1)) begin
+			if (we_p) begin// write data vao database
+					addrp=addr;
+					dp=0;
+					dp[SIZE_1-1:0]=data;
+					we_p=0; // write unable pixel (database)
+			end
+			i=i+1;
+		end
+		if ((i>lastaddr-firstaddr)&&(sh==1)) begin
+			step=step+1;          //next step
+			i=0;
+		end
+	end
+	if ((step_out==2)||(step_out==4)||(step_out==6)||(step_out==8)||(step_out==10)||(step_out==12)||(step_out==14)) begin
+		if ((i<=lastaddr-firstaddr)&&(sh==0)) begin
+			addr=i1;
+		end
+		if ((i<=lastaddr-firstaddr)&&(sh==1)) begin
+			we_w=0;
+			addrw=addr;
+			if (weight_case!=0) i=i+1; 
+			if (step_out==14) if (i_d==(in_dense)) begin  
+				dw=buff; 
+				we_w=1; 
+				weight_case=1; 
+				i_d=0; 
+				i1=i1+1; 
+			end
+			case (weight_case)
+				0: ;
+				1: begin buff=0; buff[SIZE_9-1:SIZE_8]=data; end   
+				2: buff[SIZE_8-1:SIZE_7]=data; 
+				3: buff[SIZE_7-1:SIZE_6]=data;  
+				4: buff[SIZE_6-1:SIZE_5]=data;  
+				5: buff[SIZE_5-1:SIZE_4]=data; .
+				6: buff[SIZE_4-1:SIZE_3]=data;  
+				7: buff[SIZE_3-1:SIZE_2]=data;  
+				8: buff[SIZE_2-1:SIZE_1]=data;   
+				9: begin buff[SIZE_1-1:0]=data;  i1=i1+1; end
+				default: $display("Check weight_case");
+			endcase
+			if (weight_case!=0) i_d=i_d+1;
+			if (weight_case==9) begin 
+				weight_case=1; 
+				dw=buff; 
+				we_w=1; 
+			end else weight_case=weight_case+1;
+		end
+
+		if ((i>lastaddr-firstaddr)&&(sh==1)) begin
+			step=step+1;          //next step
+			i=0;
+			i_d=0;
+			i1=0;
+			weight_case=0;
+		end
+
+	end else we_w=0;
+ end
 endmodule
